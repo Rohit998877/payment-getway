51344dd49fd101ea9c0655a680e26e4244886a48
DevelopRohit <rohitmohadikar897@gmail.com>
2026-03-15 23:48:47 +0530

    render

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Rohit@789",
+    host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+    user="4SH4E9PNTGGfHry.root",
+    password="11jBBKuhqgO0plgI",
     database="payment_system"
 )
 # pip install -r requirements.txt
